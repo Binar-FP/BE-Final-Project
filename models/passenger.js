f04ea618a0a9de30797d8d143216bfee946c1592
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2022-11-25 20:01:44 +0800

    add table Passenger

---
+++ models/passenger.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Passenger extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Passenger.init({
+    passengerId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    NIK: DataTypes.BIGINT,
+    phoneNumber: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Passenger',
+  });
+  return Passenger;
+};
