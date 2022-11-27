c7f16dad353f2c02a343256323e2d8f5049354f1
Setya Rizky Pradana <setyarizkypradana@gmail.com>
2022-11-27 21:33:54 +0700

    fix migration airport and flight

--- db/migrations/20221125121618-create-flight.js
+++ db/migrations/20221125121618-create-flight.js
@@ -1,65 +1,55 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Flights', {
+    await queryInterface.createTable("Flights", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       flightId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       airLine: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       fromId: {
-        type: Sequelize.CHAR
+        type: Sequelize.CHAR,
       },
       toId: {
-        type: Sequelize.CHAR
+        type: Sequelize.CHAR,
       },
       depatureTime: {
-        type: Sequelize.TIME
+        type: Sequelize.TIME,
       },
       arrivalTime: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       capasity: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       economyClassPrice: {
-        type: Sequelize.DOUBLE
+        type: Sequelize.DOUBLE,
       },
-<<<<<<< HEAD:migrations/20221125065750-create-ticket-airplane.js
-      typeOfTicket: {
-        type: Sequelize.ENUM("Economy Class", "Business Class", "First Class")
-=======
       businessClassPrice: {
-        type: Sequelize.DOUBLE
->>>>>>> d40ea0003805d5a35b944ae5d1118c8d452179a2:db/migrations/20221125121618-create-flight.js
+        type: Sequelize.DOUBLE,
       },
       firstClassPrice: {
-        type: Sequelize.DOUBLE
+        type: Sequelize.DOUBLE,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-<<<<<<< HEAD:migrations/20221125065750-create-ticket-airplane.js
-  async down(queryInterface) {
-    await queryInterface.dropTable('TicketAirplanes');
-=======
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Flights');
->>>>>>> d40ea0003805d5a35b944ae5d1118c8d452179a2:db/migrations/20221125121618-create-flight.js
-  }
+    await queryInterface.dropTable("Flights");
+  },
 };
