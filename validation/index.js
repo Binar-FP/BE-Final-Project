6192f9da9b76faf3e3484525af5fa09e73ae85cf
Setya Rizky Pradana <setyarizkypradana@gmail.com>
2022-11-28 13:43:03 +0700

    fix validation

--- validation/index.js
+++ validation/index.js
@@ -1,30 +1,31 @@
 /* eslint-disable global-require */
 /* eslint-disable */
-const {check, validationResult} = require('express-validator')
+const { check, validationResult } = require("express-validator");
+const { User } = require("../app/models");
 // const { register } = require('../app/controllers/registerController')
 
-
 // running validator
+
 runValidation = (req, res, next) => {
-  const errors = validationResult(req)
-  if(!errors.isEmpty()){
-    return res.status(404).json({
-      message: errors.array()[0].msg
-    })
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array()[0].msg,
+    });
   }
-  next()
-}
+  next();
+};
 
 registerValidation = [
-  check('username', 'username not Empty').notEmpty(),
-  check('email', 'email not Empty').notEmpty().matches(/.+\@.+\..+/).withMessage('email required use @'),
-  check('password', 'password not Empty').notEmpty().isLength({min: 8 }).withMessage('password minimum 8 character'),
-]
-
-
-
+  check("username", "username is required").notEmpty(),
+  check("email", "email is required")
+    .notEmpty()
+    .matches(/.+\@.+\..+/)
+    .withMessage("This email is invalid. Make sure it's written like  example@email.com"),
+  check("password", "password is required").notEmpty().isLength({ min: 8 }).withMessage("Your password is too short"),
+];
 
 module.exports = {
   runValidation,
-  registerValidation
-}
+  registerValidation,
+};
