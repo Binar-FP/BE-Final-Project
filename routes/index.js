ad66b51be4d42f99fbf6b5bbcaabb87e1f570c97
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-28 22:05:25 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const register = require("../app/controllers/registerController");
 const login = require("../app/controllers/loginController");
 const { runValidation, registerValidation } = require("../validation");
 
+console.log('test')
 router.post("/api/register", registerValidation, runValidation, register.register);
 router.post("/login", login.signin);
 
