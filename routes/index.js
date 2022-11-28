3e6de0978e8737ad38fb80ccbe7eb7931743910c
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-28 22:31:15 +0700

    fix conflict

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const register = require("../app/controllers/registerController");
 const login = require("../app/controllers/loginController");
 const { runValidation, registerValidation } = require("../validation");
-console.log('test')
+
 router.post("/api/register", registerValidation, runValidation, register.register);
 router.post("/login", login.signin);
 
