f8bee94a4e66635d7082fd58c1e8bf8fb979950e
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2022-11-29 13:20:21 +0800

    add seeders

---
+++ seeders/20221129051813-admin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Admins', [{
+      email: 'zakir@gmail.com',
+      password: '0205',
+      firstName: "Nadir",
+      lastName: "Nawi",
+      roleId: "buyer",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
