fa0bf01d42573e31253ed70d4e23dae2d8c12c88
ZidanBagus <zsetiawan02@gmail.com>
2022-11-29 16:25:50 +0700

    set up swagger

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
 
+// import package swagger 
+const swaggerUi = require('swagger-ui-express');
+// import file json
+const swaggerDocument = require('../docs/swagger.json');
+// api docs
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
 const register = require("../app/controllers/registerController");
 const login = require("../app/controllers/loginController");
 const { runValidation, registerValidation } = require("../validation");
