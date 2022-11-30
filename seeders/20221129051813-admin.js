1543b6dafbfbd705503b0906f472ffdfe111ef7b
Setya Rizky Pradana <setyarizkypradana@gmail.com>
2022-11-30 13:30:38 +0700

    add bcrypt for password

--- seeders/20221129051813-admin.js
+++ seeders/20221129051813-admin.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
+
+const bcrypt = require("bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,24 +13,26 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-     await queryInterface.bulkInsert('Admins', [{
-      email: 'zakir@gmail.com',
-      password: '0205',
-      firstName: "Nadir",
-      lastName: "Nawi",
-      roleId: "admin",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }])
+     */
+    await queryInterface.bulkInsert("Admins", [
+      {
+        email: "zakir@gmail.com",
+        password: bcrypt.hashSync("0205", 10),
+        firstName: "Nadir",
+        lastName: "Nawi",
+        roleId: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
