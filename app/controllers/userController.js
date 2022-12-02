a58c17533dd89daf7c730df51528a53d8dad8942
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2022-12-02 18:09:13 +0800

    update swagger

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,11 +1,11 @@
 const { User } = require("../models");
-const imagekit = require('../../lib/imageKit')
-
 
 async function findUsers(req, res) {
   try {
     const responseData = await User.findAll();
     res.status(200).json({
+      status: "success",
+      meesage: "success get all users",
       data: responseData,
     });
   } catch (error) {
@@ -17,6 +17,8 @@ async function findUsersById(req, res) {
   try {
     const responseData = await User.findByPk(req.params.id);
     res.status(200).json({
+      status: "success",
+      meesage: "success get user by id",
       data: responseData,
     });
   } catch (error) {
@@ -27,7 +29,10 @@ async function findUsersById(req, res) {
 async function deleteUsers(req, res) {
   try {
     await User.destroy({ where: { id: req.params.id } });
-    res.status(200).json({ message: "User has been deleted sucessfully" });
+    res.status(200).json({
+      status: "success",
+      message: "User has been deleted sucessfully",
+    });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
@@ -35,33 +40,38 @@ async function deleteUsers(req, res) {
 
 async function updateUserById(req, res) {
   try {
-    const { password, firstName, lastName, NIK, address, phoneNumber, image, dateOfBirth, gender } = req.body;
+    const {
+      password,
+      firstName,
+      lastName,
+      NIK,
+      address,
+      phoneNumber,
+      image,
+      dateOfBirth,
+      gender,
+    } = req.body;
 
-    const file = req.file
-
-    const validFormat = file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/gif';
-    if (!validFormat) {
-        return res.status(400).json({
-            status: 'failed',
-            message: 'Wrong Image Format'
-        })
-    }
-
-    const split = file.originalname.split('.')
-        const ext = split[split.length - 1]
-
-    const img = await imagekit.upload({
-        file: file.buffer, 
-        fileName: `IMG-${Date.now()}.${ext}`, 
-      })
-  
     await User.update(
-      { password, firstName, lastName, NIK, address, phoneNumber, image: img.url, dateOfBirth, gender },
+      {
+        password,
+        firstName,
+        lastName,
+        NIK,
+        address,
+        phoneNumber,
+        image,
+        dateOfBirth,
+        gender,
+      },
       {
         where: { id: req.params.id },
       }
     );
-    res.status(200).json({ message: "Your profile has been update sucessfully" });
+    res.status(200).json({
+      status: "success",
+      message: "Your profile has been update sucessfully",
+    });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
