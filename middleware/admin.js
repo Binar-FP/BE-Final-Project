dded71131dc3d17444e798724125dac34ef354f4
Setya Rizky Pradana <setyarizkypradana@gmail.com>
2022-12-04 22:07:19 +0700

    feat role access for admin

---
+++ middleware/admin.js
@@ -0,0 +1,12 @@
+const admin = (req, res, next) => {
+  if (req.admin.roleId == "admin") {
+    next();
+  } else {
+    res.status(401).json({
+      status: "failed",
+      message: "You don't have permission to access this page",
+    });
+  }
+};
+
+module.exports = admin;
---
+++ middleware/authentication.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const { Admin, User } = require("../app/models");
+const { ACCES_TOKEN_SECRET } = process.env;
+
+const authAdmin = (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+    const token = bearerToken.split("Bearer ")[1];
+    if (!token) {
+      return res.status(401).json({
+        status: "failed",
+        message: "Required header authorization",
+      });
+    }
+
+    const payload = jwt.verify(token, ACCES_TOKEN_SECRET);
+    Admin.findByPk(payload.id).then((instance) => {
+      req.admin = instance;
+      next();
+    });
+  } catch {
+    res.status(401).json({
+      status: "failed",
+      message: "Invalid Token",
+    });
+  }
+};
+
+const authUser = (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+    const token = bearerToken.split("Bearer ")[1];
+    if (!token) {
+      return res.status(401).json({
+        status: "failed",
+        message: "Required header authorization",
+      });
+    }
+
+    const payload = jwt.verify(token, ACCES_TOKEN_SECRET);
+    User.findByPk(payload.id).then((instance) => {
+      req.user = instance;
+      next();
+    });
+  } catch {
+    res.status(401).json({
+      status: "failed",
+      message: "Invalid Token",
+    });
+  }
+};
+
+module.exports = { authAdmin, authUser };
