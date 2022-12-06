e8f56d20eca7b3ef61e15e3c6f6c4e39452afdcd
ZidanBagus <zsetiawan02@gmail.com>
2022-12-06 09:52:10 +0700

    add api whislist_destination

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const auth = require("../routes/auth");
 const airports = require("../routes/airports");
 const users = require("../routes/users");
 const docs = require("../routes/docs");
+const destination = require("../routes/destinations");
 
 router.use(cors());
 router.use("/api/", auth);
 router.use("/api/airports/", airports);
 router.use("/api/users/", users);
+router.use("/api/destinations/", destination);
 router.use("/api-docs", docs);
 
 module.exports = router;
