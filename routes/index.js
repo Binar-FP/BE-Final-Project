eb3a186236b04cf730972666e29c780710c37c15
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2022-12-07 09:53:36 +0800

    refactor

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const auth = require("../routes/auth");
 const airports = require("../routes/airports");
 const users = require("../routes/users");
 const docs = require("../routes/docs");
-const flights = require("../routes/flights")
+const flights = require("../routes/flights");
 const destination = require("../routes/destinations");
 const passengers = require("../routes/passengers");
 
@@ -14,11 +14,8 @@ router.use("/api/", auth);
 router.use("/api/airports/", airports);
 router.use("/api/users/", users);
 router.use("/api/destinations/", destination);
-<<<<<<< HEAD
 router.use("/api/passenger/", passengers);
-=======
 router.use("/api/flights/", flights);
->>>>>>> 61a4607c4c87ec22b6c6f14894b19f6fbcbe50f4
 router.use("/api-docs", docs);
 
 module.exports = router;
