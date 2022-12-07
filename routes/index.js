a0b8c26b455f9697e8880b6a10db14ed17fa637d
ZidanBagus <zsetiawan02@gmail.com>
2022-12-07 08:10:52 +0700

    add api schedule

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,16 @@ const docs = require("../routes/docs");
 const flights = require("../routes/flights")
 const destination = require("../routes/destinations");
 const passengers = require("../routes/passengers");
+const Schedules = require("../routes/schedules")
 
 router.use(cors());
 router.use("/api/", auth);
 router.use("/api/airports/", airports);
 router.use("/api/users/", users);
 router.use("/api/destinations/", destination);
-<<<<<<< HEAD
 router.use("/api/passenger/", passengers);
-=======
 router.use("/api/flights/", flights);
->>>>>>> 61a4607c4c87ec22b6c6f14894b19f6fbcbe50f4
+router.use("/api/schedule/", Schedules);
 router.use("/api-docs", docs);
 
 module.exports = router;
