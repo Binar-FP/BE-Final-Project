20a78287f897cdce1de82a342aaa4c7106521686
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-11 13:55:21 +0700

    set more config for test in github action

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
