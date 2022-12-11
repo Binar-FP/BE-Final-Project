240bbd067dca28550f96942f945ce15333f4355c
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2022-12-12 02:43:27 +0800

    approve test eslint

--- routes/passengers.js
+++ routes/passengers.js
@@ -6,8 +6,8 @@ const admin = require("../middleware/admin")
 const { authAdmin, } = require("../middleware/authentication")
 
 router.post("/add", authAdmin, admin, passengers.addPassenger)
-router.get("/findAll", passengers.findPassengers);
-router.get("/findById/:id", passengers.findPassengersById);
+router.get("/findAll", passengers.findPassengers)
+router.get("/findById/:id", passengers.findPassengersById)
 router.delete("/delete/:id", authAdmin, admin, passengers.deletePasenger)
 router.put("/update/:id", authAdmin, admin, passengers.updatePassengerById)
 
