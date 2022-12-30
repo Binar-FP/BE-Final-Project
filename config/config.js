bfc500f13155095881930a6d0fe1fff7875c9d44
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2022-12-30 23:40:33 +0700

    fixing config database

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, } = process.env
 const pg = require("pg")
 
@@ -11,14 +12,18 @@ const sharedConfig = {
 
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: process.env.DATABASE_URL,
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync('./config/ca-certificate.crt').toString(),
+      }
+    }
   },
   test: {
     username: DB_USERNAME,
