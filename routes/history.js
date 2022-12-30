b1fb54d395518f6ad73608f34d69becd4c059881
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2022-12-30 09:14:31 +0700

    add route for get all history

--- routes/history.js
+++ routes/history.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
-const { getHistory, } = require("../app/controllers/historyController")
+const { getHistory, getAllHistory} = require("../app/controllers/historyController")
 
 router.get("/histories", getHistory)
+router.get("/histories/all", getAllHistory )
 
 module.exports = router
