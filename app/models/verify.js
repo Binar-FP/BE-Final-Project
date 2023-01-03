97fdce9cab54a2271cfe8946e2cb241ae6e23594
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2023-01-03 16:10:19 +0700

    modify models verify

--- app/models/verify.js
+++ app/models/verify.js
@@ -9,6 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      this.belongsTo(models.User, {
+        foreignKey: {
+          name: "userId",
+          allowNull: false,
+        },
+      });
     }
   }
   Verify.init(
